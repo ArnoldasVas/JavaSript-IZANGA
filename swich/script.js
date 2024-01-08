7a4ef0d6d261a419a807f8108bea1e39997d5236
Arnoldas <arnoldvasiliauskas@gmail.com>
2024-01-08 22:23:23 +0200

    javaScript pradzia

---
+++ swich/script.js
@@ -0,0 +1,13 @@
+let carColor = 'red';
+
+switch (carColor) {
+  case 'red':
+    console.log('car is red');
+    break;
+  case 'green':
+    console.log('car is green');
+    break;
+  default:
+    console.log('car is yelov');
+    break;
+}
